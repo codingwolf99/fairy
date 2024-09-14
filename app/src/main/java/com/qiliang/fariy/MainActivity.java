8357c7f2ebd1b404b0885c9c5040580393046a0e
huangkuncan <huangkuncan@meituan.com>
2024-09-14 16:03:36 +0800

    install app 配置

--- app/src/main/java/com/qiliang/fariy/MainActivity.java
+++ app/src/main/java/com/qiliang/fariy/MainActivity.java
@@ -3,7 +3,7 @@ package com.qiliang.fariy;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,16 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.qiliang.fariy.callback.NetCallback;
 import com.qiliang.fariy.model.DataResponse;
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(com.qiliang.fariy.R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(com.qiliang.fariy.R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
